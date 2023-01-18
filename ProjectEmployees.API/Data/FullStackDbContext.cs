eb154a2ba57f8c115621933fcfdea786baa33c6b
Bartłomiej Kiljański <bartlomiej.kiljanski@aplitt.pl>
2023-01-19 00:00:00 +0100

    Dodaj pliki projektów.

---
+++ ProjectEmployees.API/Data/FullStackDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectEmployees.API.Models;
+
+namespace ProjectEmployees.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
